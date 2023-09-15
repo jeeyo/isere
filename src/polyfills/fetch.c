6bc2d18582212c78a22f865fd7b533c5ce3d2450
jeeyo <jeeyo@outlook.com>
2023-09-15 21:00:05 +0700

    some progress on fetch

--- src/polyfills/fetch.c
+++ src/polyfills/fetch.c
@@ -5,6 +5,17 @@
 #include "quickjs.h"
 #include "quickjs-libc.h"
 
+JSValue polyfill_fetch_internal(JSContext *ctx, JSValueConst this_val, int argc, JSValueConst *argv)
+{
+  int64_t delay;
+  JSValueConst url, method;
+
+  url = argv[0];
+  method = argv[1];
+
+  return JS_TRUE;
+}
+
 void polyfill_fetch_init(JSContext *ctx)
 {
   /*
@@ -22,30 +33,31 @@ void polyfill_fetch_init(JSContext *ctx)
     }
     ```
   */
+
+  JSValue global_obj = JS_GetGlobalObject(ctx);
+  JS_SetPropertyStr(ctx, global_obj, "__fetch", JS_NewCFunction(ctx, polyfill_fetch_internal, "__fetch", 1));
+  JS_FreeValue(ctx, global_obj);
+
+  // TODO: support standard Fetch Request object (https://developer.mozilla.org/en-US/docs/Web/API/Request)
   const char *str = "globalThis.fetch = async function(resource) {\n"
-                    "  return resource;\n"
+                    "  let url = null\n"
+                    "  if (typeof resource === 'string') url = resource\n"
+                    "  else if (typeof resource === 'object' && typeof resource.url === 'string') url = resource.url\n"
+                    "  else if (typeof resource === 'object') url = resource.toString()\n"
+
+                    "  let method = 'GET'\n"
+                    "  if (typeof resource === 'object' && typeof resource.method === 'string') method = resource.method\n"
+
+                    "  return globalThis.__fetch(url, method);\n"
                     "};";
-  JS_Eval(ctx, str, strlen(str), "<fetch>", JS_EVAL_TYPE_MODULE);
+  JS_Eval(ctx, str, strlen(str), "<polyfills>", JS_EVAL_TYPE_MODULE);
 }
 
 void polyfill_fetch_deinit(JSContext *ctx)
 {
-}
-
-JSValue polyfill_fetch_internal(JSContext *ctx, JSValueConst this_val, int argc, JSValueConst *argv)
-{
-  int64_t delay;
-  JSValueConst res;
-  JSValue obj;
-
-  res = argv[0];
-
-  // `resource` must be an object
-  if (!JS_IsObject(res)) {
-    return JS_ThrowTypeError(ctx, "not an object");
-  }
-
-  return obj;
+  JSValue global_obj = JS_GetGlobalObject(ctx);
+  JS_DeleteProperty(ctx, global_obj, JS_NewAtom(ctx, "__fetch"), 0);
+  JS_FreeValue(ctx, global_obj);
 }
 
 int polyfill_fetch_poll(JSContext *ctx)
