080cc64f91021efec508ba374b6b55b95437d9c2
jeeyo <jeeyo@outlook.com>
2023-09-21 18:44:15 +0700

    go

--- src/js.rs
+++ src/js.rs
@@ -1,12 +1,10 @@
-use rquickjs::{Context, Runtime, context::intrinsic, Object, Function, String as QjsString};
-use core::error::Error;
+use rquickjs::{Context, Runtime, Module, context::intrinsic, Object, Function, loader::{BuiltinResolver, BuiltinLoader}};
+use core::{error::Error};
 use core::fmt;
-use heapless::{String, Vec};
+use log::{error, info, warn};
 
-const MAX_HTTP_HEADERS: usize = 20;
-const MAX_HTTP_HEADER_NAME_LEN: usize = 64;
-const MAX_HTTP_HEADER_VALUE_LEN: usize = 1024;
-const MAX_HTTP_BODY_LEN: usize = 2048;
+extern crate alloc;
+use alloc::{string::String, vec::Vec, format};
 
 /*
   Response object:
@@ -22,21 +20,26 @@ const MAX_HTTP_BODY_LEN: usize = 2048;
   }
   ```
 */
+#[derive(Debug)]
 pub struct Response {
   is_base64_encoded: bool,
   status_code: u32,
-  headers: Vec<(String<MAX_HTTP_HEADER_NAME_LEN>, String<MAX_HTTP_HEADER_VALUE_LEN>), MAX_HTTP_HEADERS>,
-  body: String<2048>,
+  headers: Vec<(String, String)>,
+  body: String,
 }
+
+// impl fmt::Display for Response {
+//   fn fmt(&self, 
+// }
 
 #[derive(Debug)]
 pub struct JsError {
-  message: String<128>,
+  message: String,
 }
 
 impl fmt::Display for JsError {
   fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-    write!(f, "")
+    write!(f, "{}", self.message)
   }
 }
 
@@ -46,18 +49,15 @@ impl Error for JsError {
   }
 }
 
-type Callback = fn();
-
 pub struct Js {
-  rt: Runtime,
   ctx: Context,
-  callback: Callback,
 }
 
 impl Js {
-  const ISERE_JS_HANDLER_FUNCTION_RESPONSE_OBJ_NAME: &'static str = "__response";
+  pub const ISERE_JS_STACK_SIZE: usize = 65536;
+  pub const ISERE_JS_HANDLER_FUNCTION_RESPONSE_OBJ_NAME: &'static str = "__response";
 
-  pub fn init(callback: Callback) -> Result<Js, JsError> {
+  pub fn init() -> Result<Js, JsError> {
     let rt = match Runtime::new() {
       Ok(rt) => rt,
       Err(_e) => return Err(JsError { message: String::from("failed to create QuickJS runtime") }),
@@ -80,19 +80,49 @@ impl Js {
         Err(_e) => return Err(JsError { message: String::from("failed to create QuickJS context") }),
     };
 
-    // rt.set_max_stack_size(limit);
+    // TODO: dynamically link this
+    let resolver = BuiltinResolver::default().with_module("handler");
+    let loader = BuiltinLoader::default().with_module("handler",
+      "
+export const handler = async function(event, context, done) {
+  console.log('Test ESM')
+  console.log('## ENVIRONMENT VARIABLES: ', process.env)
+  console.log('## CONTEXT: ', context)
+  console.log('## EVENT: ', event)
+
+  // setTimeout(() => {
+  //   console.log('ESM Inside')
+  // }, 5000)
+
+  // done({
+  //   statusCode: 200,
+  //   headers: { 'Content-Type': 'text/plain' },
+  //   body: { k: 'j' }
+  // })
+
+  // const a = await new Promise(resolve => resolve('555'));
+  // console.log('a', a)
+
+  return {
+    statusCode: 404,
+    headers: { 'Content-Type': 'text/plain' },
+    body: { k: 'v' }
+  }
+}
+      ");
+
+    rt.set_loader(resolver, loader);
+
+    // TODO: set global memory limit with JS_SetMemoryLimit()
+    rt.set_max_stack_size(Self::ISERE_JS_STACK_SIZE);
 
     ctx.with(|ctx| {
       // add console.log(), console.warn(), and console.error() function
-      let console = Object::new(ctx).unwrap();
+      let console = Object::new(ctx.clone()).unwrap();
 
-      // TODO: no_std logging
-      // let log = Function::new(ctx.clone(), |msg: QjsString| { info!("{}", msg); }).unwrap();
-      // let warn = Function::new(ctx.clone(), |msg: QjsString| { warn!("{}", msg); }).unwrap();
-      // let error = Function::new(ctx.clone(), |msg: QjsString| { error!("{}", msg); }).unwrap();
-      let log = Function::new(ctx.clone(), |_msg: QjsString| { }).unwrap();
-      let warn = Function::new(ctx.clone(), |_msg: QjsString| { }).unwrap();
-      let error = Function::new(ctx.clone(), |_msg: QjsString| { }).unwrap();
+      let log = Function::new(ctx.clone(), |msg: String| { info!("{msg}"); }).unwrap().with_name("log").unwrap();
+      let warn = Function::new(ctx.clone(), |msg: String| { warn!("{msg}"); }).unwrap().with_name("warn").unwrap();
+      let error = Function::new(ctx.clone(), |msg: String| { error!("{msg}"); }).unwrap().with_name("error").unwrap();
 
       console.set("log", log).unwrap();
       console.set("warn", warn).unwrap();
@@ -100,11 +130,11 @@ impl Js {
       ctx.globals().set("console", console).unwrap();
 
       // add process.env
-      let envvars = Object::new(ctx).unwrap();
+      let envvars = Object::new(ctx.clone()).unwrap();
       // envvars.set("NODE_ENV", env::var("NODE_ENV").unwrap_or("Development".to_string())).unwrap();
       envvars.set("NODE_ENV", "Production").unwrap();
 
-      let process = Object::new(ctx).unwrap();
+      let process = Object::new(ctx.clone()).unwrap();
       process.set("env", envvars).unwrap();
       ctx.globals().set("process", process).unwrap();
 
@@ -113,75 +143,50 @@ impl Js {
       // TOOD: fetch polyfill
     });
 
-    Ok(Js { rt, ctx, callback })
+    Ok(Js { ctx })
   }
 
-  pub fn eval(&self, _js: &str) -> Result<Response, JsError> {
-    let val = match self.ctx.with(|ctx| -> Result<Response, JsError> {
-      // TODO: dynamically link this
-      let handler = match ctx.compile("handler",
-      "export const handler = async function(event, context, done) {
-          console.log('Test ESM')
-          console.log('## ENVIRONMENT VARIABLES: ', process.env)
-          console.log('## CONTEXT: ', context)
-          console.log('## EVENT: ', event)
-
-          setTimeout(() => {
-            console.log('ESM Inside')
-          }, 5000)
-
-          // done({
-          //   statusCode: 200,
-          //   headers: { 'Content-Type': 'text/plain' },
-          //   body: { k: 'j' }
-          // })
-
-          const a = await new Promise(resolve => resolve('555'));
-          console.log('a', a)
-
-          return {
-            statusCode: 404,
-            headers: { 'Content-Type': 'text/plain' },
-            body: { k: 'v' }
-          }
-        }
-
-        console.log('ESM Outside')"
+  pub fn eval(&self) -> Result<(), JsError> {
+    match self.ctx.with(|ctx| -> Result<(), JsError> {
+      match Module::evaluate(ctx, "<isere>",
+        "
+import { handler } from 'handler'
+globalThis.__test = { 'iam': 'jeeyo' }
+console.log('test')
+const handler1 = new Promise(async resolve => resolve(await handler(__event, __context, resolve)))
+Promise.resolve(handler1).then((response) => {
+  globalThis.__response = response;
+  console.log('response');
+})
+console.log('bro');
+        "
       ) {
-        Ok(handler) => handler,
-        Err(_e) => return Err(JsError { message: String::from("failed to compile handler module") }),
-      };
-
-      unsafe {
-        match handler.eval() {
-          Ok(_) => (),
-          Err(_e) => return Err(JsError { message: String::from("failed to evaluate handler module") }),
-        }
-      };
-
-      let val: Object = ctx.eval(
-        "import { handler } from 'handler';
-        const handler1 = new Promise(async resolve => resolve(await handler(__event, __context, resolve)))
-        Promise.resolve(handler1).then(cb)"
-      ).unwrap();
-
-      let headers = val.get::<_, Object>("headers").unwrap().props();
-
-      Ok(Response {
-        is_base64_encoded: val.get::<_, bool>("isBase64Encoded").unwrap(),
-        status_code: val.get::<_, u32>("statusCode").unwrap(),
-        headers: val.get::<_, Object>("headers").unwrap()
-          .props()
-          .collect::<Result<Vec<(String<MAX_HTTP_HEADER_NAME_LEN>, String<MAX_HTTP_HEADER_VALUE_LEN>), MAX_HTTP_HEADERS>, _>>()
-          .unwrap(),
-        body: val.get::<_, String<MAX_HTTP_BODY_LEN>>("body").unwrap(),
-      })
+        Ok(_) => return Ok(()),
+        Err(e) => return Err(JsError { message: format!("failed to evaluate module: {}", e) }),
+      }
     }) {
-      Ok(val) => val,
-      Err(e) => return Err(e),
+      Ok(_) => (),
+      Err(e) => { return Err(e); },
     };
 
-    let _ = self.rt.execute_pending_job();
-    Ok(val)
+    let _ = self.ctx.runtime().execute_pending_job().unwrap();
+
+    self.ctx.with(|ctx| {
+      info!("{:?}", ctx.globals().get::<_, Object>("__test").unwrap().get::<_, String>("iam").unwrap());
+    });
+
+    // let resp = self.ctx.with(|ctx| -> Response {
+    //   Response {
+    //     is_base64_encoded: module.get::<_, bool>("isBase64Encoded").unwrap_or(false),
+    //     status_code: module.get::<_, u32>("statusCode").unwrap_or(200),
+    //     headers: module.get::<_, Object>("headers").unwrap_or(Object::new(ctx.clone()).unwrap())
+    //       .props()
+    //       .collect::<Result<Vec<(String, String)>, _>>()
+    //       .unwrap(),
+    //     body: module.get::<_, String>("body").unwrap_or(String::from("")),
+    //   }
+    // });
+
+    Ok(())
   }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,37 @@
 #![no_std]
 #![feature(error_in_core)]
+#![feature(core_panic)]
+
+#[global_allocator]
+static GLOBAL: FreeRtosAllocator = FreeRtosAllocator;
 
 mod js;
-mod log;
+
+use freertos_rust::*;
+use log::{LevelFilter, error};
 
 use js::Js;
 
 fn main() {
-  let js = match Js::init(|| {}) {
+  env_logger::Builder::new()
+    .filter(None, LevelFilter::Info)
+    .init();
+
+  let js = match Js::init() {
     Ok(js) => js,
-    Err(e) => panic!("{}", e),
+    Err(e) => {
+      error!("{}", e);
+      return;
+    },
   };
+
+  match js.eval() {
+    Ok(_) => (),
+    Err(e) => {
+      error!("{}", e);
+      return;
+    },
+  }
+
+  // println!("{:?}", response);
 }
