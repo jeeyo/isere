cf6be599d69317656dd267fadd879112e71bfc16
jeeyo <jeeyo@outlook.com>
2024-03-10 02:07:26 +0700

    porting to cmake

--- portable/src/tcp.c
+++ portable/src/tcp.c
@@ -4,8 +4,8 @@
 #include <unistd.h>
 #include <errno.h>
 
-#include <sys/socket.h>
-#include <arpa/inet.h>
+// #include <sys/socket.h>
+// #include <arpa/inet.h>
 
 int tcp_init()
 {
@@ -19,79 +19,84 @@ int tcp_deinit()
 
 int tcp_socket_new()
 {
-  int sock = socket(AF_INET, SOCK_STREAM, IPPROTO_IP);
-  if (sock < 0) {
-    return -1;
-  }
+  // int sock = socket(AF_INET, SOCK_STREAM, IPPROTO_IP);
+  // if (sock < 0) {
+  //   return -1;
+  // }
 
-  return sock;
+  // return sock;
+  return -1;
 }
 
 void tcp_socket_close(int sock)
 {
-  close(sock);
+  // close(sock);
 }
 
 int tcp_listen(int sock, uint16_t port)
 {
-  struct sockaddr_in dest_addr;
-  bzero(&dest_addr, sizeof(dest_addr));
-  dest_addr.sin_family = AF_INET;
-  dest_addr.sin_addr.s_addr = htonl(INADDR_ANY);
-  dest_addr.sin_port = htons(port);
+  // struct sockaddr_in dest_addr;
+  // bzero(&dest_addr, sizeof(dest_addr));
+  // dest_addr.sin_family = AF_INET;
+  // dest_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+  // dest_addr.sin_port = htons(port);
 
-  int err = bind(sock, (struct sockaddr *)&dest_addr, sizeof(dest_addr));
-  if (err != 0) {
-    return -1;
-  }
+  // int err = bind(sock, (struct sockaddr *)&dest_addr, sizeof(dest_addr));
+  // if (err != 0) {
+  //   return -1;
+  // }
 
-  err = listen(sock, 1);
-  if (err != 0) {
-    return -1;
-  }
+  // err = listen(sock, 1);
+  // if (err != 0) {
+  //   return -1;
+  // }
 
-  return 0;
+  // return 0;
+  return -1;
 }
 
 int tcp_accept(int sock, char *ip_addr)
 {
-  struct sockaddr_in source_addr;
-  socklen_t addr_len = sizeof(source_addr);
+  // struct sockaddr_in source_addr;
+  // socklen_t addr_len = sizeof(source_addr);
 
-  int fd = accept(sock, (struct sockaddr *)&source_addr, &addr_len);
-  if (fd < 0 && errno == EINTR) {
-    return -2;
-  }
+  // int fd = accept(sock, (struct sockaddr *)&source_addr, &addr_len);
+  // if (fd < 0 && errno == EINTR) {
+  //   return -2;
+  // }
 
-  if (fd < 0) {
-    return -1;
-  }
+  // if (fd < 0) {
+  //   return -1;
+  // }
 
-  // disable tcp keepalive
-  int keep_alive = 0;
-  setsockopt(fd, SOL_SOCKET, SO_KEEPALIVE, &keep_alive, sizeof(int));
+  // // disable tcp keepalive
+  // int keep_alive = 0;
+  // setsockopt(fd, SOL_SOCKET, SO_KEEPALIVE, &keep_alive, sizeof(int));
 
-  // copy ip address string
-  strncpy(ip_addr, inet_ntoa(((struct sockaddr_in *)&source_addr)->sin_addr), INET_ADDRSTRLEN);
+  // // copy ip address string
+  // strncpy(ip_addr, inet_ntoa(((struct sockaddr_in *)&source_addr)->sin_addr), INET_ADDRSTRLEN);
 
-  return fd;
+  // return fd;
+  return -1;
 }
 
 int tcp_recv(int sock, char *buf, size_t len)
 {
-  int recvd = recv(sock, buf, len, 0);
-  if (recvd < 0) {
-    if (errno == EINTR) {
-      return -2;
-    }
+  // int recvd = recv(sock, buf, len, 0);
+  // if (recvd < 0) {
+  //   if (errno == EINTR) {
+  //     return -2;
+  //   }
 
-    return -1;
-  }
+  //   return -1;
+  // }
 
-  return recvd;
+  // return recvd;
+  return -1;
 }
 
 int tcp_write(int sock, const char *buf, size_t len)
 {
-  return write(sock, buf, len);
+  // return write(sock, buf, len);
+  return -1;
 }
