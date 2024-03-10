7933c248b58235b47c9b52b0d4346dbba6e4e66b
jeeyo <jeeyo@outlook.com>
2024-03-10 23:23:09 +0700

    cmake for binary

---
+++ portable/linux/src/tcp.c
@@ -0,0 +1,97 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <errno.h>
+
+#include <sys/socket.h>
+#include <arpa/inet.h>
+
+int tcp_init()
+{
+  return 0;
+}
+
+int tcp_deinit()
+{
+  return 0;
+}
+
+int tcp_socket_new()
+{
+  int sock = socket(AF_INET, SOCK_STREAM, IPPROTO_IP);
+  if (sock < 0) {
+    return -1;
+  }
+
+  return sock;
+}
+
+void tcp_socket_close(int sock)
+{
+  close(sock);
+}
+
+int tcp_listen(int sock, uint16_t port)
+{
+  struct sockaddr_in dest_addr;
+  bzero(&dest_addr, sizeof(dest_addr));
+  dest_addr.sin_family = AF_INET;
+  dest_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+  dest_addr.sin_port = htons(port);
+
+  int err = bind(sock, (struct sockaddr *)&dest_addr, sizeof(dest_addr));
+  if (err != 0) {
+    return -1;
+  }
+
+  err = listen(sock, 1);
+  if (err != 0) {
+    return -1;
+  }
+
+  return 0;
+}
+
+int tcp_accept(int sock, char *ip_addr)
+{
+  struct sockaddr_in source_addr;
+  socklen_t addr_len = sizeof(source_addr);
+
+  int fd = accept(sock, (struct sockaddr *)&source_addr, &addr_len);
+  if (fd < 0 && errno == EINTR) {
+    return -2;
+  }
+
+  if (fd < 0) {
+    return -1;
+  }
+
+  // disable tcp keepalive
+  int keep_alive = 0;
+  setsockopt(fd, SOL_SOCKET, SO_KEEPALIVE, &keep_alive, sizeof(int));
+
+  // copy ip address string
+  strncpy(ip_addr, inet_ntoa(((struct sockaddr_in *)&source_addr)->sin_addr), INET_ADDRSTRLEN);
+
+  return fd;
+}
+
+int tcp_recv(int sock, char *buf, size_t len)
+{
+  int recvd = recv(sock, buf, len, 0);
+  if (recvd < 0) {
+    if (errno == EINTR) {
+      return -2;
+    }
+
+    return -1;
+  }
+
+  return recvd;
+}
+
+int tcp_write(int sock, const char *buf, size_t len)
+{
+  return write(sock, buf, len);
+}
---
+++ portable/rp2040/src/logger.c
@@ -0,0 +1,80 @@
+#include "logger.h"
+
+#include <stdarg.h>
+#include <stdio.h>
+
+static SemaphoreHandle_t _stdio_mut = NULL;
+static StaticSemaphore_t _stdio_mutbuf;
+
+static void __logger_print(FILE *fp, const char *tag, isere_log_level_t level, const char *fmt, va_list vargs)
+{
+  xSemaphoreTake(_stdio_mut, portMAX_DELAY);
+
+  // TO-DO: log level
+  fprintf(fp, "[%s] ", tag);
+  vfprintf(fp, fmt, vargs);
+  fprintf(fp, "\n");
+  fflush(stdout);
+
+  xSemaphoreGive(_stdio_mut);
+}
+
+static void logger_error(const char *tag, const char *fmt, ...)
+{
+  va_list vargs;
+  va_start(vargs, fmt);
+  __logger_print(stderr, tag, LOG_LEVEL_ERROR, fmt, vargs);
+  va_end(vargs);
+}
+
+static void logger_warning(const char *tag, const char *fmt, ...)
+{
+  va_list vargs;
+  va_start(vargs, fmt);
+  __logger_print(stdout, tag, LOG_LEVEL_WARNING, fmt, vargs);
+  va_end(vargs);
+}
+
+static void logger_info(const char *tag, const char *fmt, ...)
+{
+  va_list vargs;
+  va_start(vargs, fmt);
+  __logger_print(stdout, tag, LOG_LEVEL_INFO, fmt, vargs);
+  va_end(vargs);
+}
+
+static void logger_debug(const char *tag, const char *fmt, ...)
+{
+  va_list vargs;
+  va_start(vargs, fmt);
+  __logger_print(stdout, tag, LOG_LEVEL_DEBUG, fmt, vargs);
+  va_end(vargs);
+}
+
+int logger_init(isere_t *isere, isere_logger_t *logger)
+{
+  (void)isere;
+
+  if (_stdio_mut == NULL) {
+    _stdio_mut = xSemaphoreCreateMutexStatic(&_stdio_mutbuf);
+  }
+
+  *logger = (isere_logger_t){
+    .error = &logger_error,
+    .warning = &logger_warning,
+    .info = &logger_info,
+    .debug = &logger_debug,
+  };
+
+  return 0;
+}
+
+void logger_deinit(isere_logger_t *logger)
+{
+  (void)logger;
+
+  if (_stdio_mut) {
+    vSemaphoreDelete(_stdio_mut);
+    _stdio_mut = NULL;
+  }
+}
