f6b4e3ca4ca1d3e038681a04c5656fc0a7578f44
jeeyo <jeeyo@outlook.com>
2024-03-29 00:19:57 +0700

    lwip socket no_sys=0

--- portable/rp2040/init.cmake
+++ portable/rp2040/init.cmake
@@ -1,5 +1,5 @@
 include(3rdparty/pico-sdk/pico_sdk_init.cmake)
 
 set(NEED_FREERTOS_POSIX OFF)
-set(NEED_LWIP ON)
+set(NEED_LWIP OFF)  # use included LwIP in pico-sdk
 set(SUPPORT_DYNLINK OFF)
--- portable/rp2040/link.cmake
+++ portable/rp2040/link.cmake
@@ -4,12 +4,20 @@ target_include_directories(isere PRIVATE
   ${PICO_TINYUSB_PATH}/lib/networking
 )
 
-target_link_libraries(isere pico_stdlib hardware_exception pico_unique_id tinyusb_device lwipallapps lwipcore)
+target_link_libraries(isere
+  pico_stdlib
+  pico_lwip
+  pico_lwip_freertos
+  pico_lwip_contrib_freertos
+  hardware_exception
+  pico_unique_id
+  tinyusb_device
+)
+
+target_compile_options(isere PRIVATE -fno-math-errno)
 
 pico_enable_stdio_usb(isere 0)
 pico_enable_stdio_uart(isere 0)
-
-target_compile_definitions(isere PRIVATE PICO_ENTER_USB_BOOT_ON_EXIT=1)
 
 # create map/bin/hex file etc.
 pico_add_extra_outputs(isere)
