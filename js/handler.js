b2ee070bb97bc320716d0a939efaaf936cfcecaa
jeeyo <jeeyo@outlook.com>
2024-09-10 05:27:29 +0700

    restore handler.js

--- js/handler.js
+++ js/handler.js
@@ -1,5 +1,27 @@
-var http = require('http');
+export const handler = async function(event, context, done) {
+  console.log('Test ESM')
+  console.log('## ENVIRONMENT VARIABLES: ', process.env)
+  console.log('## CONTEXT: ', context)
+  console.log('## EVENT: ', event)
 
-http.createServer(function (req, res) {
-  res.end(JSON.stringify({ k: 'v' }));
-}).listen(8080);
+  setTimeout(() => {
+    console.log('ESM Inside')
+  }, 1000)
+
+  // done({
+  //   statusCode: 200,
+  //   headers: { 'Content-Type': 'text/plain' },
+  //   body: { k: 'j' }
+  // })
+
+  const a = await new Promise(resolve => resolve('555'));
+  console.log('a', a)
+
+  return {
+    statusCode: 200,
+    headers: { 'Content-Type': 'text/plain' },
+    body: { k: 'v' }
+  }
+}
+
+console.log('ESM Outside')
