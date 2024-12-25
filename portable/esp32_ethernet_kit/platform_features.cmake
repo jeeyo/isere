2aaebd366c129d2536b667c8e1fd0a4fbf39769f
Natchanon Nuntanirund <jeeyo@outlook.com>
2024-12-25 16:59:47 +0000

    esp tools

--- portable/esp32_ethernet_kit/platform_features.cmake
+++ portable/esp32_ethernet_kit/platform_features.cmake
@@ -5,5 +5,6 @@ SET(JERRY_GLOBAL_HEAP_SIZE "(70)" CACHE STRING "")
 
 set(ESPIDF_DIR 3rdparty/esp-idf)
 include(${ESPIDF_DIR}/tools/cmake/project.cmake)
+
 # "Trim" the build. Include the minimal set of components, main, and anything it depends on.
 idf_build_set_property(MINIMAL_BUILD ON)
