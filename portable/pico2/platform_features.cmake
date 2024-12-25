0e9f6bf97f26b1cb28d583332914cf2e0bd9bae0
Natchanon Nuntanirund <jeeyo@outlook.com>
2024-12-25 15:42:05 +0000

    Cleanup

--- portable/pico2/platform_features.cmake
+++ portable/pico2/platform_features.cmake
@@ -5,4 +5,9 @@ include(3rdparty/pico-sdk/pico_sdk_init.cmake)
 set(WITH_LWIP OFF)  # use included LwIP in pico-sdk
 set(SUPPORT_DYNLINK OFF)
 
+if(WITH_OTEL)
+  set(WITH_OTEL OFF)
+  message(WARNING "Target 'pico2' doesn't support OpenTelemetry -- disabled")
+endif()
+
 SET(JERRY_GLOBAL_HEAP_SIZE "(70)" CACHE STRING "")
